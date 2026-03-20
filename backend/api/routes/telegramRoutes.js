6a46bc6a3e7eca25901b424e9e3d003cf2c553b4
YourGitHubUsername <kiptoo445120@gmail.com>
2026-03-20 04:28:38 +0300

    Move server.js to root, restructure backend folder

--- backend/api/routes/telegramRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const telegramController = require('../controllers/telegramController');
-
-router.post('/send-message', telegramController.sendMessage);
-router.get('/get-updates/:offset?', telegramController.getUpdates);
-
-module.exports = router;
